c70868708257c911d5eb89502d5b57b646fdc787
minaabdullayeva <minaabdullayeva125>
2024-10-18 16:31:20 +0400

    Created

---
+++ weaponn.cs/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace weaponn.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Weapon weapon = null;
+
+            while (true)
+            {
+                if (weapon == null)
+                {
+                    Console.WriteLine("Daragin gulle tutumu ve atis sayini daxil edin");
+                    Console.Write("Gulle tutumu: ");
+                    int capacity = int.Parse(Console.ReadLine());
+                    Console.Write("Gulle sayi: ");
+                    int bulletCount = int.Parse(Console.ReadLine());
+                    Console.Write("atis modu daxil edin tekli ve ya avto: ");
+                    string fireMode = Console.ReadLine();
+
+                    weapon = new Weapon(capacity, bulletCount, fireMode);
+                }
+
+                Console.WriteLine("menyu:");
+                Console.WriteLine("informasiya almaq ucun 0 dAXIL EDIN");
+                Console.WriteLine("1. SHOOT metodonu ise salin");
+                Console.WriteLine("2 - GetRemainBulletCount metodu ise salin");
+                Console.WriteLine("3 - Reload metodu ise salin");
+                Console.WriteLine("4 - ChangeFireMode metodu ise salin");
+                Console.WriteLine("5 - Proqramdan dayandırmaq ucun 5 daxil edin");
+                
+                int operation = int.Parse(Console.ReadLine());
+
+                switch (operation)
+                {
+                    case 0:
+                        Console.WriteLine($"Güllə tutumu: {weapon.Capacity}, Güllə sayı: {weapon.BulletCount}, Atış modu: {weapon.FireMode}");
+                        do { Console.WriteLine($"Gulle tutumu: {weapon.Capacity}, Gulle sayi: {weapon.BulletCount}, Atış modu: {weapon.FireMode}"); } while (weapon.Capacity > weapon.BulletCount);
+                        break;
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        Console.WriteLine($"Tam dolması üçün lazım olan güllə sayı: {weapon.GetRemainBulletCount()}");
+                        break;
+                    case 3:
+                        weapon.Reload();
+                        break;
+                    case 4:
+                        weapon.ChangeFireMode();
+                        break;
+                    case 5:
+                        Console.WriteLine("Program dayandirilir");
+                        return;
+
+                    default:
+                        Console.WriteLine("duzgun secim edin");
+                        break;
+                }
+            }
+        }
+    }
+}
+
---
+++ weaponn.cs/weapon.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace weaponn.cs
+{
+    internal class Weapon
+    {
+        private int _capacity;
+        private int _bulletCount;
+        private string _fireMode;
+
+        public int Capacity
+        {
+          
+            get { return _capacity; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _capacity = value;
+                }
+                else { Console.WriteLine("capacity menfi ola bilmez"); }
+            }
+        }
+        public int BulletCount { get { return _bulletCount; } set { if (value >= 0) { _bulletCount = value; } 
+                else { Console.WriteLine("bulletcount menfi ola bilmez"); } } }
+        public string FireMode { get { return _fireMode; } set { if (value == "tekli" || value == "avto") {
+                    _fireMode = value;
+                } } }
+
+        public Weapon(int capacity, int bulletCount, string fireMode)
+        {
+            if (capacity <= 0 || bulletCount < 0 || bulletCount > capacity)
+            {
+                Console.WriteLine("daraqin gulle tutumini duzgun daxil edin");
+            }
+
+            Capacity = capacity;
+            BulletCount = bulletCount;
+            FireMode = fireMode;
+        }
+
+        public void Shoot()
+        {
+            if (FireMode == "tekli")
+            {
+                if (BulletCount >= 0)
+                {
+                    BulletCount--;
+                    Console.WriteLine($" bir gulle atildi qalan gulle sayi:{BulletCount}");
+                }
+                else
+                {
+                    Console.WriteLine("daraqda gulle yoxdur");
+                }
+            }
+            else if (FireMode == "avto")
+            {
+                if (BulletCount > 0)
+                {
+                    Console.WriteLine($"{BulletCount}eded gulle atildi qalan gulle sayi 0");
+                    BulletCount = 0;
+                }
+                else
+                {
+                    Console.WriteLine("Daraqda gulle yixdur");
+                }
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return Capacity - BulletCount;
+        }
+
+        public void Reload()
+        {
+            BulletCount = Capacity;
+            Console.WriteLine("Daraq zarajat olundu");
+        }
+
+        public void ChangeFireMode()
+        {
+            FireMode = (FireMode == "tekli") ? "avto" : "tekli";
+            Console.WriteLine($"atis modu deyisildi {FireMode}");
+        }
+    }
+
+}
+
